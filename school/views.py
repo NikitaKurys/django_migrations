1b05dfa4c5f6b781f1e2ed72ec2cd915b7444281
NikitaKurys <kurys.nik@mail.ru>
2021-12-14 23:02:08 +0300

    first_commit

---
+++ school/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students = Student.objects.all()
+    context = {
+        'students': students,
+    }
+
+    ordering = 'group'
+
+    return render(request, template, context)
